24bcd2001a94ed97dd2cf8b5d35f8f97184da5ba
Akash Debnath <66691792+Akash37iit@users.noreply.github.com>
2022-02-10 12:58:41 +0600

    Add files via upload

---
+++ ChatBotDemo.java
@@ -0,0 +1,11 @@
+
+package ai_lab_final;
+
+public class ChatBotDemo {                                     //Driver Class
+
+	public static void main(String[] args) {             //main class
+		
+          new Chatbot();                                  // instantiation 
+	}
+
+}
